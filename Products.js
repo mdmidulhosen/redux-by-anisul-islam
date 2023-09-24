14dacf861791525bfd98d1288a10d639903f2c29
Mridul Hosen <muhammadmridulhosenkibria@gmail.com>
2023-09-24 13:14:34 +0600

    tutorial 7 done

--- Products.js
+++ Products.js
@@ -1,52 +1,95 @@
-const { legacy_createStore } = require("redux");
+const { legacy_createStore, combineReducers } = require("redux");
 
 // constans
 const GET_PRODUCTS = "GET_PRODUCTS";
 const ADD_PRODUCT = "ADD_PRODUCT";
 
+const GET_CART = "GET_CART";
+const ADD_CART = "ADD_CART";
 
 const initialState = {
-    products: ["salt", "sugar"],
-    count: 2
-}
+  products: ["salt", "sugar"],
+  count: 2,
+};
+
+const initialCartState = {
+    cart: ["mango", "banana"],
+    count: 2,
+  };
 
 // ACTION
 const getProducts = () => {
-    return{
-        type: GET_PRODUCTS,
-    }
-}
+  return {
+    type: GET_PRODUCTS,
+  };
+};
 const addProduct = (product) => {
-    return{
-        type: ADD_PRODUCT,
-        payload: product
-    }
-}
+  return {
+    type: ADD_PRODUCT,
+    payload: product,
+  };
+};
 
-const productReducer = (state = initialState, action)=> {
+const getCart = () => {
+    return {
+      type: GET_CART,
+    };
+  };
+  const addCart = (cart) => {
+    return {
+      type: ADD_CART,
+      payload: cart,
+    };
+  };
+
+const productReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_PRODUCTS:
+      return {
+        ...state,
+      };
+    case ADD_PRODUCT:
+      return {
+        products: [...state.products, action.payload],
+        count: state.count + 1,
+      };
+
+    default:
+     return state;
+  }
+};
+
+const cartReducer = (state = initialCartState, action) => {
     switch (action.type) {
-        case GET_PRODUCTS:
-            
-            return{
-                ...state
-            }
-        case ADD_PRODUCT:
-            
-            return{
-                products: [...state.products, action.payload],
-                count: state.count + 1
-            }
-    
-        default:
-            state;
+      case GET_CART:
+        return {
+          ...state,
+        };
+      case ADD_CART:
+        return {
+          cart: [...state.cart, action.payload],
+          count: state.count + 1,
+        };
+  
+      default:
+        return state;
     }
-}
+  };
 
-const store = legacy_createStore(productReducer)
+  const rootReducer = combineReducers({
+    productR: productReducer,
+    cartR: cartReducer,
+  })
 
-store.subscribe(()=> {
-    console.log(store.getState())
-})
-store.dispatch(getProducts())
-store.dispatch(addProduct("rice"))
-store.dispatch(addProduct("milk"))
+const store = legacy_createStore(rootReducer);
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+store.dispatch(getProducts());
+store.dispatch(addProduct("rice"));
+store.dispatch(addProduct("milk"));
+
+store.dispatch(getCart());
+store.dispatch(addCart("rice"));
+store.dispatch(addCart("milk"));
